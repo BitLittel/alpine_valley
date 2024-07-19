9fb19bb91b292a4ce0424858fd85cf52348c0bc6
BitLittel <opera.operaciy@yandex.ru>
2024-07-19 18:01:15 +0700

    finish back

--- main/api/submit.py
+++ main/api/submit.py
@@ -7,7 +7,5 @@ from main.schemas.response import DefaultResponse
 
 @main.post('/api/submit', response_model=DefaultResponse, status_code=201, tags=['Submit'])
 async def api_submit(form_submit: Submit, request: Request, response: Response, token=Cookie(default=None)):
-    print(form_submit, request)
-    token = None
     await process_submit(form_submit=form_submit, request=request, token=token, response=response)
     return {'result': True, 'message': 'Вы успешно оставили заявку, с вами позже свяжутся', 'data': {}}
