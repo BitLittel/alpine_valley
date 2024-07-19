c3f6ff75f6de46f1586a89b008fd17e5413220c7
BitLittel <opera.operaciy@yandex.ru>
2024-07-19 16:35:18 +0700

    finish back

--- main/__init__.py
+++ main/__init__.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 main = FastAPI(title='Bubble')
 main.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://альпийскиеимения.рф/"
+]
+
+main.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 from main.views import views
