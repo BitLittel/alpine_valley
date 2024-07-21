2c42f7e2c45b79f1c7ab21dcb674eb9fefd2d29e
BitLittel <opera.operaciy@yandex.ru>
2024-07-21 13:38:46 +0700

    finish back

--- main/__init__.py
+++ main/__init__.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import time
+from datetime import datetime
 
 main = FastAPI(title='Bubble')
 main.mount("/static", StaticFiles(directory="static"), name="static")
@@ -23,6 +25,19 @@ main.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+@main.middleware("http")
+async def before_request(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    response.headers["Cache-Control"] = 'no-cache, no-store, must-revalidate, max-age=0'
+    response.headers["Pragma"] = 'no-cache'
+    response.headers["Expires"] = '0'
+    response.headers["Last-Modified"] = datetime.now().isoformat()
+    return response
 
 
 from main.views import views
